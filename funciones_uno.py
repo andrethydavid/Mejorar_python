19d213b46bbdafdfd5d853e9df4fa0bf4f23a6c7
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-05-31 21:17:48 -0500

    funciones commit "fbasic funcion"

---
+++ funciones_uno.py
@@ -0,0 +1,20 @@
+print('hola')
+
+def my_print(text):
+  print( text * 2)
+ 
+
+my_print('este es mi texto')
+my_print('hola ')
+
+
+a = 10
+b = 90
+
+c = a + b 
+
+def suma( a, b):
+ my_print ( a + b)
+
+suma (1, 5) 
+suma(10,4) 
